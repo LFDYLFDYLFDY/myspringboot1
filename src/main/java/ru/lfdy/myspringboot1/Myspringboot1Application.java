099835eb7169382bb043b31b2570c6124eed4845
LFDY <lfdy@yandex.ru>
2024-07-08 14:58:42 +0300

    445454

---
+++ src/main/java/ru/lfdy/myspringboot1/Myspringboot1Application.java
@@ -0,0 +1,13 @@
+package ru.lfdy.myspringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myspringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myspringboot1Application.class, args);
+	}
+
+}
