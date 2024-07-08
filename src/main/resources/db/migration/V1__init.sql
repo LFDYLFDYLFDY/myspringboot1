5e9692d847ca9b7fcb479ba2fa8ffbeb33a77e19
LFDY <lfdy@yandex.ru>
2024-07-08 16:17:44 +0300

    3333333

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
-CREATE TABLE products (id serial, title varchar(100), price int);
-
-INSERT INTO products (title, price) VALUES ('Bread', 40), ('Milk', 80);
+-- CREATE TABLE products (id serial, title varchar(100), price int);
+--
+-- INSERT INTO products (title, price) VALUES ('Bread', 40), ('Milk', 80);
 
 CREATE TABLE users (
     username VARCHAR(50) NOT NULL,
