d04d41a9f5392128dd22d2704c00455eac153470
LFDY <lfdy@yandex.ru>
2024-07-09 13:31:13 +0300

    5

--- src/main/java/ru/lfdy/myspringboot1/controllers/MainController.java
+++ src/main/java/ru/lfdy/myspringboot1/controllers/MainController.java
@@ -1,7 +1,9 @@
 package ru.lfdy.myspringboot1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,9 +13,15 @@ public class MainController {
        return "index";
     }
     @GetMapping("/hello")
-        public String helloRequest(){
+        public String helloRequest(Model model){
+        model.addAttribute("name", "BOB");
          return "hello";
         }
+   @GetMapping("/hello_new")
+   public String helloNew(Model model, @RequestParam(value = "name", required = false) String name){
+        model.addAttribute("name", name);
+        return "hello";
+   }
    @GetMapping("/cats")
    @ResponseBody
      public Cat catRequest(){
