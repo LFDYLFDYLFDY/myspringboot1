96c15a62d14ab28576cb9afe1906e1d50741a3c2
LFDY <lfdy@yandex.ru>
2024-07-09 11:38:27 +0300

    3333333

---
+++ src/main/java/ru/lfdy/myspringboot1/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package ru.lfdy.myspringboot1.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("ru.lfdy.myspringboot1")
+public class AppConfig {
+}
---
+++ src/main/java/ru/lfdy/myspringboot1/controllers/MainController.java
@@ -0,0 +1,12 @@
+package ru.lfdy.myspringboot1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/index")
+    public String doSomething(){
+       return "index";
+    }
+}
