a7b5cf2e3be13ff3d9b9a1fcd584423d0777fb78
LFDY <lfdy@yandex.ru>
2024-07-11 16:27:18 +0300

    1111

---
+++ src/main/java/ru/lfdy/myspringboot1/repositories/Item.java
@@ -0,0 +1,23 @@
+package ru.lfdy.myspringboot1.repositories;
+
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name="items")
+public class Item {
+//    @Id
+//            @GeneratedValue
+//            @Column (name = "id")
+//    private  Long id;
+//    @Column(name = "title")
+//    private  String title;
+//
+//    public Item() {
+//    }
+//
+//    public Item(Long id, String title) {
+//        this.id = id;
+//        this.title = title;
+//    }
+
+}
