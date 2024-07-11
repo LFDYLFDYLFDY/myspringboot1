8f42884b235f0843c1539d4a85520ab3b3debf68
LFDY <lfdy@yandex.ru>
2024-07-11 16:57:15 +0300

    11112

--- src/main/java/ru/lfdy/myspringboot1/repositories/Item.java
+++ src/main/java/ru/lfdy/myspringboot1/repositories/Item.java
@@ -1,24 +1,30 @@
 package ru.lfdy.myspringboot1.repositories;
 
 
-//import jakarta.persistence.*;
-//
-//@Entity
-//@Table(name="items")
-//public class Item {
-//    @Id
-//            @GeneratedValue
-//            @Column(name = "id")
-//    private  Long id;
-//    @Column(name = "title")
-//    private  String title;
-//
-//    public Item() {
-//    }
-//
-//    public Item(Long id, String title) {
-//        this.id = id;
-//        this.title = title;
-//    }
-//
-//}
+import org.springframework.data.annotation.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="items")
+public class Item {
+    @Id
+            @GeneratedValue
+            @Column(name = "id")
+    private  Long id;
+    @Column(name = "title")
+    private  String title;
+
+    public Item() {
+    }
+
+    public Item(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+}
