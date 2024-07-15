beabcfbffd25c6040b9e32c20b949a9e6fd694ba
LFDY <lfdy@yandex.ru>
2024-07-15 10:51:48 +0300

    11113333

--- src/main/java/ru/lfdy/myspringboot1/repositories/Item.java
+++ src/main/java/ru/lfdy/myspringboot1/repositories/Item.java
@@ -2,18 +2,18 @@ package ru.lfdy.myspringboot1.repositories;
 
 
 import org.springframework.data.annotation.*;
+import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
 @Table(name="items")
 public class Item {
+    @javax.persistence.Id
     @Id
-            @GeneratedValue
+//            @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
             @Column(name = "id")
     private  Long id;
     @Column(name = "title")
@@ -27,4 +27,11 @@ public class Item {
         this.title = title;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
